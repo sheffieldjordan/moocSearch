d8de32169c1c633a159061e18a254c8d7874e07d
sheffieldjordan <sheffieldjordan@gmail.com>
2016-11-22 05:28:32 -0800

    NEW app.js

--- app.js
+++ app.js
@@ -5,23 +5,22 @@ var request = require('request')  // Adding the request library (to make HTTP re
 var tools = require('./tools.js'); // Custom module providing additional functionality to the server
 var bodyParser = require('body-parser');
 
-
 var app = express(); // initializing applicaiton
 
-var YouTube = require('youtube-node');
-var youTube = new YouTube();
-youTube.setKey('AIzaSyASBTPnt-OJlLc3ey0SMhiU38a2THSsnP4');
+// var YouTube = require('youtube-node');
+// var youTube = new YouTube();
+// youTube.setKey('AIzaSyASBTPnt-OJlLc3ey0SMhiU38a2THSsnP4');
 
-var khan = require('khan');
+// var khan = require('khan');
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: false }));
 // For each request to this server, run the function "logger" in tools.js 
-app.use(tools.logger);
+// app.use(tools.logger);
 
-// Set up /static path to host css/js/image files directly
+// Set up /static path to host css/js/image files directly 
 app.use('/static', express.static(__dirname + '/static'));
 
 /****END OF NO NEED TO MODIFY SECTION ******/
@@ -33,10 +32,16 @@ app.get('/', function (req, res, next) {
 });
 
 //Upon submitting form, send get request to MOOCS APIs
-app.get('/search', function(req,res,next){
-  var name = req.params.name;
-  var query = req.params.query;
-  var results = [] //array to add up all results (can also be a different format, what works best)
+app.post('/', function(req,res,next){
+  var name = req.body.name;
+  var courses = req.body.courses;
+  var results = req.body.searchoptions
+  // var results = [] //array to add up all results (can also be a different format, what works best)
+  console.log("Variables");
+  console.log(name);
+  console.log(courses);
+  console.log(results);
+
 
 // Add requests for each API that is checked
 
@@ -44,22 +49,22 @@ app.get('/search', function(req,res,next){
 //Add if-statement for checked or not 
 
 //request to youTube API
-youTube.search(query, 2, function(error, result, body) {
-  if (error) {
-    console.log(error);
-  }
-  else {
-    console.log(JSON.stringify(result, null, 2));
-    json_body = JSON.parse(body);
-    var videoId = json_body.data[1].result;
-    //var content = json_body.data[0].content; 
+// youTube.search(query, 2, function(error, result, body) {
+//   if (error) {
+//     console.log(error);
+//   }
+//   else {
+//     console.log(JSON.stringify(result, null, 2));
+//     json_body = JSON.parse(body);
+//     var videoId = json_body.data[1].result;
+//     //var content = json_body.data[0].content; 
 
-    //this rendering should only be done after all results are gathered
-    // res.render("contactus.html", {
-    video_search_results: "https://www.youtube.com/watch?v=" + videoId;
-    //  });
-    }
-  });
+//     //this rendering should only be done after all results are gathered
+//     // res.render("contactus.html", {
+//     video_search_results: "https://www.youtube.com/watch?v=" + videoId;
+//     //  });
+//     }
+});
 
 
 //EdX
@@ -74,11 +79,11 @@ youTube.search(query, 2, function(error, result, body) {
 //Add if-statement for checked or not 
 //Add request to API here
 
-function getVideo(query) {
-  var khan_results = khan.topicVideos(query);
-  console.log(khan_results);
+// function getVideo(query) {
+//   var khan_results = khan.topicVideos(query);
+//   console.log(khan_results);
 
-}
+// }
 
 //Coursera 
 //Add if-statement for checked or not here
@@ -91,8 +96,8 @@ function getVideo(query) {
 
 
 //Render page with all results 
-res.render("home.html", {results: 'results'})
-});
+// res.render("home.html", {results: 'results'})
+// });
 
 
 // Start up server on port 3000 on host localhost
