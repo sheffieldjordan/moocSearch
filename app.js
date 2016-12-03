75544cf4b92c180ed915c43baf011a2c5169239a
Jolly <jollylst@hotmail.com>
2016-12-02 19:15:33 -0800

    Add handlers for app.js and changed html (deleted edx and name)

--- app.js
+++ app.js
@@ -65,29 +65,36 @@ app.post('/', function(req,res,next){
     if(options[i] === 'youtube'){
       console.log("Search YouTube");
       console.log(i);
+
       youTube.search(courses, 5, function(error, result, body) {
         var youTubeResp = result;
         if (error) {
           console.log(error);
-        }
-        else {
+        } else {
           //console.log(JSON.stringify(result, null, 5));
           json_body = JSON.stringify(youTubeResp);
           json_parsed = JSON.parse(json_body);
+
           if (json_parsed.pageInfo.totalResults == 0){
-            console.log("RETURNED NO RESULTSSSSSSSSSSSSSSS!!!!");
+            console.log("Youtube Returned No Results!");           
+          } else {
+            var max_len = 0; //initialize the max_len for cappping the results
             
+            if (json_parsed.pageInfo.totalResults >= 5) {
+              max_len = 5; 
+            } else {
+              max_len = json_parsed.pageInfo.totalResults;
+            }
+
+            for(var i = 0; i < max_len; i++) {
+              var youTubeUrl = "https://www.youtube.com/embed/" + json_parsed.items[i].id.videoId;
+              var youTubeTitle = json_parsed.items[i].snippet.title;
+              var youTubeDescr = json_parsed.items[i].snippet.description;
+              results.push(["YouTube: " + youTubeTitle, youTubeUrl, youTubeDescr]);
+            }
           }
-          else{
-             for(var i = 0; i < 5; i++) {
-            var youTubeUrl = "https://www.youtube.com/embed/" + json_parsed.items[i].id.videoId;
-            var youTubeTitle = json_parsed.items[i].snippet.title;
-            var youTubeDescr = json_parsed.items[i].snippet.description;
-            results.push(["YouTube: " + youTubeTitle, youTubeUrl, youTubeDescr]);
-          }
-          }       
-         
         }
+
       //Check if this is the last search task before rendering all the results?
         if (async_tokens === 1) {
           res.render("home.html", {'results': results,'results_image': results_image});
@@ -96,19 +103,6 @@ app.post('/', function(req,res,next){
       });
     }
 
-    //EdX
-    //Check if this MOOC provider is checked
-    if(options[i] === 'edX'){
-      console.log("Search edX");
-      console.log(i);
-      //Add request to API here
-      
-      //Check if this is the last search task before rendering all the results?
-      if (async_tokens === 1) {
-        res.render("home.html", {'results': results});
-      }
-      async_tokens--;   //One Task done: decrements the token.
-    }
 
     //Udemy
     //Check if this MOOC provider is checked
@@ -128,16 +122,28 @@ app.post('/', function(req,res,next){
               //console.log(response.statusCode);
               json_body = JSON.parse(body);
               // console.log(json_body);
-              
-              for(var i=0; i<5; i++) {
-                var name = json_body.results[i].title;
-                var courseUrl = 'https://www.udemy.com' + json_body.results[i].url;
-                var imageUrl = json_body.results[i].image_480x270;
-                var description = json_body.results[i].description;
-                //console.log(name, courseUrl, imageUrl);
-                results_image.push(["Udemy: " + name, courseUrl, imageUrl, description]);
+
+              if (json_body.results.length == 0) {
+                console.log('Udemy Returned No Results!');
+              } else {
+                var max_len = 0; //initialize the max_len for cappping the results
+
+                if (json_body.results.length >= 5) {
+                  max_len = 5; 
+                } else {
+                  max_len = json_body.results.length;
+                }
+            
+                for(var i =0 ; i < max_len; i++) {
+                  var name = json_body.results[i].title;
+                  var courseUrl = 'https://www.udemy.com' + json_body.results[i].url;
+                  var imageUrl = json_body.results[i].image_480x270;
+                  var description = json_body.results[i].description;
+                  //console.log(name, courseUrl, imageUrl);
+                  results_image.push(["Udemy: " + name, courseUrl, imageUrl, description]);
+                }
+                //console.log(results);
               }
-              //console.log(results);
             }
 
             //Check if this is the last search task before rendering all the results?
@@ -155,26 +161,35 @@ app.post('/', function(req,res,next){
       console.log(i);
       youTube.khanSearch(courses, 5, function(error, result, body) {
         var khanResp = result;
+
         if (error) {
           console.log(error);
-        }
-        else {
+        } else {
           //console.log(JSON.stringify(result, null, 5));
           json_body = JSON.stringify(khanResp);
           json_parsed = JSON.parse(json_body);
           //console.log(json_body);
+
           if (json_parsed.pageInfo.totalResults == 0){
-            console.log("RETURNED NO RESULTSSSSSSSSSSSSSSS!!!!");
+            console.log('Khan Academy Returned No Results!');           
+          } else {
+            var max_len = 0; //initialize the max_len for cappping the results
             
+            if (json_parsed.pageInfo.totalResults >= 5) {
+              max_len = 5; 
+            } else {
+              max_len = json_parsed.pageInfo.totalResults;
+            }
+
+            for (var i = 0; i < max_len; i++) {
+              var khanUrl = "https://www.youtube.com/embed/" + json_parsed.items[i].id.videoId;
+              var khanTitle = json_parsed.items[i].snippet.title;
+              var khanDescr = json_parsed.items[i].snippet.description;
+              results.push(["Khan Academy: " + khanTitle, khanUrl, khanDescr]);
+            }
           }
-          else {
-          for (var i = 0; i < 5; i++) {
-            var khanUrl = "https://www.youtube.com/embed/" + json_parsed.items[i].id.videoId;
-            var khanTitle = json_parsed.items[i].snippet.title;
-            var khanDescr = json_parsed.items[i].snippet.description;
-            results.push(["Khan Academy: " + khanTitle, khanUrl, khanDescr]);
-          }}
         }
+
       //Check if this is the last search task before rendering all the results?
         if (async_tokens === 1) {
           res.render("home.html", {'results': results,'results_image': results_image});
@@ -188,6 +203,7 @@ app.post('/', function(req,res,next){
     if(options[i] === 'coursera'){
       console.log("Search Coursera");
       console.log(i);
+
       var coursera_results = [];
       var query_url = 'https://api.coursera.org/api/courses.v1?q=search&query='+courses+'&includes=name,description,photoUrl,previewLink&fields=name,description,photoUrl,previewLink';
 
@@ -200,20 +216,31 @@ app.post('/', function(req,res,next){
           } else {
               //console.log(response.statusCode);
               json_body = JSON.parse(body);
-              console.log(json_body);
+              // console.log(json_body);
               
+              if (json_body.elements.length == 0) {
+                  console.log('Coursera Returned No Results!');
+                } else {
+                  var max_len = 0; //initialize the max_len for cappping the results
 
-              for (var i in json_body.elements) {
-                var name = json_body.elements[i].name;
-                var courseUrl = json_body.elements[i].previewLink;
-                var imageUrl = json_body.elements[i].photoUrl;
-                var description = json_body.elements[i].description;
-                //console.log(name, courseUrl, imageUrl, description);
-                results_image.push(["Coursera: " + name, courseUrl, imageUrl, description]);
-                }
-                console.log(results);
-                console.log(results_image);
-          }
+                  if (json_body.elements.length >= 5) {
+                    max_len = 5; 
+                  } else {
+                    max_len = json_body.elements.length;
+                  }
+
+                  for (var i=0; i<max_len; i++) {
+                    var name = json_body.elements[i].name;
+                    var courseUrl = json_body.elements[i].previewLink;
+                    var imageUrl = json_body.elements[i].photoUrl;
+                    var description = json_body.elements[i].description;
+                    //console.log(name, courseUrl, imageUrl, description);
+                    results_image.push(["Coursera: " + name, courseUrl, imageUrl, description]);
+                  }
+                  // console.log(results);
+                  // console.log(results_image);
+              }
+            }
 
           //Check if this is the last search task before rendering all the results?
           if (async_tokens === 1) {
@@ -225,9 +252,10 @@ app.post('/', function(req,res,next){
 
     //Udacity
     //Check if this MOOC provider is checked
-     if(options[i] === 'udacity'){
+    if(options[i] === 'udacity'){
       console.log("Search Udacity");
       console.log(i);
+
       request({
         url: 'http://localhost:3001/udacity?title=' + courses,
         method: 'GET',
@@ -239,7 +267,18 @@ app.post('/', function(req,res,next){
               json_body = JSON.parse(body);
               console.log("THIS IS THE UDATICYT RESULT!!!!!!!");
               
-              for(var i in json_body) {
+              if (json_body.length == 0){
+                console.log('Udacity Returned No Results!');           
+              } else {
+              var max_len = 0; //initialize the max_len for cappping the results
+            
+              if (json_body.length >= 5) {
+                max_len = 5; 
+              } else {
+                max_len = json_body.length;
+              }
+
+              for(var i=0; i<max_len; i++) {
                 var name = json_body[i].title;
 
                 var courseUrl = json_body[i].homepage;
@@ -249,16 +288,16 @@ app.post('/', function(req,res,next){
                 results_image.push(["Udacity: " + name, courseUrl,'http://admin.imbresources.org/photos/noImageFound.l.png',description]);
               }
                 console.log(results);
-              }
-          })
-      //Add request to API here
+            }
+          }
 
-        //Check if this is the last search task before rendering all the results?
-        if (async_tokens === 1) {
-          res.render("home.html", {'results': results,'results_image': results_image});
-        }
-        async_tokens--;   //One Task done: decrements the token.
-     }
+          //Check if this is the last search task before rendering all the results?
+          if (async_tokens === 1) {
+            res.render("home.html", {'results': results,'results_image': results_image});
+          }
+          async_tokens--;   //One Task done: decrements the token.
+      });
+    }
   }
   //End For-loop
   
