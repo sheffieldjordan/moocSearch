9c7cab4d657a3a9b316faefac70505e590ed016d
milouanne <m.feijt@berkeley.edu>
2016-12-07 12:36:36 -0800

    new fixes js

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,13 @@
+
 var searchForm = document.querySelectorAll("#searchForm")[0];
+
 if(searchForm != null){ 
 searchForm.addEventListener("submit", function(event){
 	var query = searchForm.elements.namedItem("courses").value;
 	
 	var popupText = "";
 
-	if(courses === ""){
+	if(query === ""){
 		popupText = "Please enter a query";
 		event.preventDefault();
 	}
