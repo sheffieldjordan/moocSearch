af86b5b0073263e96ed6c2e2877acd3cf2887788
milouanne <m.feijt@berkeley.edu>
2016-12-08 14:48:58 -0800

    change js to alert boxes

--- static/js/script.js
+++ static/js/script.js
@@ -11,11 +11,11 @@ searchForm.addEventListener("submit", function(event){
 
 	// Check if any query has been entered
 	if(query === ""){
-		notification1 = "Please enter a query";
+		alert("Please enter a query");
 		event.preventDefault();
 	} 
-
-	// Check if any checkbox has been checked
+	// Then check if any checkbox has been checked
+	else{
 	var checkCount = 0;
 	for(var i = 0; i < options.length && checkCount === 0; i++){
 		if (options[i].checked) {
@@ -23,9 +23,10 @@ searchForm.addEventListener("submit", function(event){
 		}
 	}
 	if(checkCount === 0){
-    	notification2 = "Please check the boxes of the MOOCs databases you want to search";
+    	alert("Please check the boxes of the MOOCs databases you want to search");
 		event.preventDefault();
   		}
+  	}
   	
 	document.getElementById("required").innerHTML = notification1 + '<br>' + notification2;	
 	});
