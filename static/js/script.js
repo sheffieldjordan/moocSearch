aa7f7e1bbae9d6805da4762d6b9ebaef17495af6
milouanne <m.feijt@berkeley.edu>
2016-12-07 16:54:32 -0800

    new update js

--- static/js/script.js
+++ static/js/script.js
@@ -4,12 +4,18 @@ var searchForm = document.querySelectorAll("#searchForm")[0];
 if(searchForm != null){ 
 searchForm.addEventListener("submit", function(event){
 	var query = searchForm.elements.namedItem("courses").value;	
+	var options = searchForm.elements.namedItem("courses").value;
+
 	var popupText = "";
 
 	if(query === ""){
 		popupText = "Please enter a query";
 		event.preventDefault();
 	} 
+	if (typeof options === 'undefined') {
+    	popupText += '\n' + "Please choose a MOOCs database you want to search";
+		event.preventDefault();
+  	}
 	else{
 		popupText = "";
 	}
