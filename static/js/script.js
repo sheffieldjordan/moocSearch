42fce1ec89d596a4dbb5e3b67b043e5916090500
milouanne <m.feijt@berkeley.edu>
2016-12-08 14:37:12 -0800

    style notifications

--- static/js/script.js
+++ static/js/script.js
@@ -9,20 +9,21 @@ searchForm.addEventListener("submit", function(event){
 	var notification1 = "";
 	var notification2 = "";
 
+	// Check if any query has been entered
 	if(query === ""){
 		notification1 = "Please enter a query";
 		event.preventDefault();
 	} 
 
+	// Check if any checkbox has been checked
 	var checkCount = 0;
-
 	for(var i = 0; i < options.length && checkCount === 0; i++){
 		if (options[i].checked) {
 			checkCount++;
 		}
 	}
 	if(checkCount === 0){
-    	notification2 = "Please choose a MOOCs database you want to search";
+    	notification2 = "Please check the boxes of the MOOCs databases you want to search";
 		event.preventDefault();
   		}
   	
